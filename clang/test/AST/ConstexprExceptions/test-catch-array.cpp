0d11a8b224084dcb981bde362f8f871e1c1a4b5b
Hana Dusíková <hanicka@hanicka.net>
2024-07-21 18:10:19 +0200

    snapshot

--- clang/test/AST/ConstexprExceptions/test-catch-array.cpp
+++ clang/test/AST/ConstexprExceptions/test-catch-array.cpp
@@ -6,6 +6,8 @@ template <typename T> consteval T test(T head, auto... tail) {
   const T array[] = {head, tail...};
   try {
     throw array;
+  } catch (const T (&arr)[5]) {
+    return -2;
   } catch (const T * ptr) {
     return *ptr;
   } catch (...) {
@@ -13,11 +15,24 @@ template <typename T> consteval T test(T head, auto... tail) {
   }
 }
 
-constexpr auto r0 = test(0,1,2,3,4);
-static_assert(r0 == 0);
+constexpr auto r0 = test(1,2,3,4,5,6);
+static_assert(r0 == 1);
 
-constexpr auto r1 = test(2,3,4);
-static_assert(r1 == 2);
+constexpr auto r1 = test(1,2,3,4,5);
+static_assert(r1 == 1);
+
+constexpr auto r2 = test(1,2,3,4);
+static_assert(r2 == 1);
+
+constexpr auto r3 = test(7,1,2,3,4,5,6);
+static_assert(r3 == 7);
+
+constexpr auto r4 = test(8,1,2,3,4,5);
+static_assert(r4 == 8);
+
+constexpr auto r5 = test(9,1,2,3,4);
+static_assert(r5 == 9);
+
 
 
 // CHECK-NOT: error
