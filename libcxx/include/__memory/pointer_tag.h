2f61c568c73d1652c0e17a6fed6e2aeeb1b6a03f
Hana Dusíková <hanicka@hanicka.net>
2024-09-28 14:38:08 +0200

    snapshot

--- libcxx/include/__memory/pointer_tag.h
+++ libcxx/include/__memory/pointer_tag.h
@@ -13,9 +13,8 @@
 #include <__config>
 #include <__type_traits/is_trivially_copyable.h>
 #include <__assert>
-#include <__fwd/tuple.h>
-#include "has_single_bit.h"
-#include "popcount.h"
+#include "__bit/has_single_bit.h"
+#include <tuple>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
 #  pragma GCC system_header
@@ -24,108 +23,92 @@
 _LIBCPP_BEGIN_NAMESPACE_STD
 
 #if _LIBCPP_STD_VER >= 26
-  
-template <unsigned _Alignment> requires (std::has_single_bit(_Alignment)) constexpr uintptr_t pointer_tag_mask = (_Alignment) - 1u;
 
-template <class _T, uintptr_t _Mask = pointer_tag_mask<alignof(_T)>> class tagged_pointer;
+template <std::unsigned_integral _T> struct pointer_mask {
+  _T value;
+};
 
-template <class _T, uintptr_t _Mask = pointer_tag_mask<alignof(_T)>> 
-[[nodiscard]] _LIBCPP_HIDE_FROM_ABI constexpr auto tag_pointer(_T * _ptr, uintptr_t _value) noexcept -> tagged_pointer<_T, _Mask> {
-#if __has_builtin(__builtin_pointer_tag)
-  auto _result = tagged_pointer<_T, _Mask>{static_cast<_T*>(__builtin_pointer_tag(_ptr, _value, _Mask))};
-#else
-  // non-constexpr variant
-  auto _result = tagged_pointer<_T, _Mask>{reinterpret_cast<T*>((reinterpret_cast<uintptr_t>(_ptr) & ~_Mask) | (_value & _Mask))};
-#endif
-  _LIBCPP_ASSERT_SEMANTIC_REQUIREMENT(_result.value() == _value, "value can't be recovered with provided mask");
-  _LIBCPP_ASSERT_SEMANTIC_REQUIREMENT(_result.pointer() == _ptr, "pointer can't be recovered with provided mask");
-  return _result;
-}
+template <unsigned _Alignment> requires (std::has_single_bit(_Alignment)) constexpr unsigned alignment_mask_v = (_Alignment) - 1u;
 
-template <uintptr_t _Mask, class _T> 
-[[nodiscard]] _LIBCPP_HIDE_FROM_ABI constexpr auto tag_pointer(_T * _ptr, uintptr_t _value) noexcept -> tagged_pointer<_T, _Mask> {
-  return tag_pointer<_T, _Mask>(_ptr, _value);
-}
+template <typename _T> constexpr unsigned alignment_mask_of_v = alignment_mask_v<alignof(_T)>;
 
-template <class _T, uintptr_t _Mask = pointer_tag_mask<alignof(_T)>> 
-[[nodiscard]] _LIBCPP_HIDE_FROM_ABI constexpr auto unsafe_tagged_pointer_cast(_T * _ptr) noexcept -> tagged_pointer<_T, _Mask> {
-  return tagged_pointer<_T, _Mask>{_ptr};
-}
+template <std::unsigned_integral T = uintptr_t> constexpr T all_bits_mask_v = ~static_cast<T>(0u); 
 
-template <class _T, uintptr_t _Mask = pointer_tag_mask<alignof(_T)>> 
-[[nodiscard]] _LIBCPP_HIDE_FROM_ABI constexpr _T * unsafe_pointer_cast(tagged_pointer<_T, _Mask> _ptr) noexcept {
-  return _ptr._ptr;
-}
-
-template <class _T, uintptr_t _Mask> 
-[[nodiscard]] _LIBCPP_HIDE_FROM_ABI constexpr _T * untag_pointer(tagged_pointer<_T, _Mask> _ptr) noexcept {
-#if __has_builtin(__builtin_pointer_untag)
-    return static_cast<_T*>(__builtin_pointer_untag(_ptr._ptr, _Mask));
-#else
-    // non-constexpr variant
-    return reinterpret_cast<T*>(reinterpret_cast<uintptr_t>(_ptr._ptr) & ~_Mask);
-#endif
-}
-
-template <class _T, uintptr_t _Mask> 
-[[nodiscard]] _LIBCPP_HIDE_FROM_ABI constexpr uintptr_t obtain_tag_value(tagged_pointer<_T, _Mask> _ptr) noexcept {
-#if __has_builtin(__builtin_pointer_tag_value)
-  return __builtin_pointer_tag_value(_ptr._ptr, _Mask);
-#else
-  // non-constexpr variant
-  return reinterpret_cast<T*>(reinterpret_cast<uintptr_t>(_ptr._ptr) & _Mask);
-#endif
-}
-
-template <class _T, uintptr_t _Mask> class tagged_pointer {
-  _T * _ptr{nullptr};
-  
-  _LIBCPP_HIDE_FROM_ABI explicit constexpr tagged_pointer(_T * _p) noexcept: _ptr{_p} { }
+template <typename _PointerT, std::unsigned_integral auto _Mask = alignment_mask_of_v<_PointerT>, std::unsigned_integral _TagT = unsigned> class pointer_tag_pair {
 public:
-  tagged_pointer() = default;
-  tagged_pointer(const tagged_pointer &) = default;
-  tagged_pointer(tagged_pointer &&) = default;
-  ~tagged_pointer() = default;
+  using pointee_type = _PointerT;
+  using pointer_type = pointee_type *;
+  using tag_type = _TagT;
+  using mask_type = decltype(_Mask);
   
-  tagged_pointer & operator=(const tagged_pointer &) = default;
-  tagged_pointer & operator=(tagged_pointer &&) = default;
+  static constexpr mask_type mask = _Mask;
+private:
+  static constexpr bool is_custom = (mask == all_bits_mask_v<mask_type>);
   
-  constexpr friend auto tag_pointer<_T, _Mask>(_T * ptr, uintptr_t _value) noexcept -> tagged_pointer<_T, _Mask>;
+  pointer_type _ptr;
   
-  constexpr friend auto unsafe_tagged_pointer_cast<_T, _Mask>(_T * ptr) noexcept -> tagged_pointer<_T, _Mask>;
-  constexpr friend _T * unsafe_pointer_cast(tagged_pointer _ptr) noexcept;
+  _LIBCPP_NODISCARD _LIBCPP_ALWAYS_INLINE static constexpr pointer_type encode_pointer_tag(pointer_type _pointer, tag_type _tag) noexcept {
+#if __has_builtin(__builtin_pointer_tag)
+    return static_cast<pointer_type>(__builtin_pointer_tag(_pointer, _tag, mask));
+#else
+    return reinterpret_cast<pointer_type>((reinterpret_cast<uintptr_t>(_pointer) & ~static_cast<uintptr_t>(mask)) | (static_cast<mask_type>(_tag) & mask));
+#endif
+  }
+  _LIBCPP_NODISCARD _LIBCPP_ALWAYS_INLINE static constexpr pointer_type decode_pointer(pointer_type _pointer) noexcept {
+#if __has_builtin(__builtin_pointer_untag)
+    return static_cast<pointer_type>(__builtin_pointer_untag(_pointer, mask));
+#else
+    static_assert(sizeof(uintptr_t) == sizeof(pointer_type));
+    return reinterpret_cast<pointer_type>(reinterpret_cast<uintptr_t>(_pointer) & reinterpret_cast<uintptr_t>(~mask));
+#endif    
+  }
+  _LIBCPP_NODISCARD _LIBCPP_ALWAYS_INLINE static constexpr tag_type decode_tag(pointer_type _pointer) noexcept {
+#if __has_builtin(__builtin_pointer_untag)
+    return static_cast<tag_type>(__builtin_pointer_tag_value(_pointer, mask));
+#else
+    static_assert(sizeof(uintptr_t) == sizeof(_pointer_t));
+    return reinterpret_cast<_pointer_t>(reinterpret_cast<uintptr_t>(_pointer) & reinterpret_cast<uintptr_t>(~mask));
+#endif    
+  }
+public:
+  constexpr pointer_tag_pair(pointer_type _pointer, tag_type _tag) noexcept requires (!is_custom): _ptr{encode_pointer_tag(_pointer, _tag)} { }
   
-  constexpr friend _T * untag_pointer<>(tagged_pointer _ptr) noexcept;
-  constexpr friend uintptr_t obtain_tag_value<>(tagged_pointer _ptr) noexcept;
+  template <typename _T = pointee_type> friend constexpr _T * pointer_cast(pointer_tag_pair _pair) noexcept requires (!is_custom){
+    return static_cast<_T *>(decode_pointer(_pair._ptr));
+  }
   
-  template <std::size_t I> [[nodiscard]] _LIBCPP_HIDE_FROM_ABI constexpr auto get() const noexcept {
-    static_assert(I <= 1, "std::tagged_pointer has only 2 elements!");
+  template <typename _T = tag_type> friend constexpr _T value_cast(pointer_tag_pair _pair) noexcept requires (!is_custom){
+    return static_cast<_T>(decode_tag(_pair._ptr));
+  }
+  
+  template <std::size_t I> friend constexpr decltype(auto) get(pointer_tag_pair _pair) noexcept {
+    static_assert(I < 2);
     if constexpr (I == 0) {
-      return untag_pointer(*this);
+      return pointer_cast(_pair);
     } else {
-      return obtain_tag_value(*this);
+      return value_cast(_pair);
     }
   }
 };
 
-template <typename T, uintptr_t Mask> struct tuple_size<tagged_pointer<T, Mask>>: std::integral_constant<std::size_t, 2> { };
+// "casting" into pointer_tag_pair
+template <std::unsigned_integral auto _Mask, typename _PointerT> constexpr auto tag_pointer(_PointerT * _ptr, std::unsigned_integral auto _tag = 0u) noexcept {
+  return pointer_tag_pair<_PointerT, _Mask, decltype(_tag)>(_ptr, _tag);
+}
 
-template<typename T, uintptr_t Mask, size_t I>
-struct tuple_element<I, tagged_pointer<T, Mask>>
-{
-    static_assert(I < 2, "std::tagged_pointer has only 2 elements!");
-};
+template <typename _PointerT, std::unsigned_integral auto _Mask = alignment_mask_of_v<_PointerT>, std::unsigned_integral _TagT = unsigned> constexpr auto tag_pointer(_PointerT * _ptr, _TagT _tag = 0u) noexcept {
+  return pointer_tag_pair<_PointerT, _Mask, _TagT>(_ptr, _tag);
+}
 
-template<typename T, uintptr_t Mask>
-struct tuple_element<0, tagged_pointer<T, Mask>>
-{
-    using type = T *;
-};
- 
-template<typename T,uintptr_t Mask>
-struct tuple_element<1, tagged_pointer<T, Mask>>
-{
-    using type = uintptr_t;
+// tuple protocol support
+template <typename _PointerT, std::unsigned_integral auto _Mask, std::unsigned_integral _TagT>
+struct tuple_size<pointer_tag_pair<_PointerT, _Mask, _TagT>>: std::integral_constant<std::size_t, 2> {};
+
+template <std::size_t I, typename _PointerT, std::unsigned_integral auto _Mask, std::unsigned_integral _TagT>
+struct tuple_element<
+    I, pointer_tag_pair<_PointerT, _Mask, _TagT>> {
+  using _pair_type = pointer_tag_pair<_PointerT, _Mask, _TagT>;
+  using type = std::conditional_t<I == 0, typename _pair_type::pointer_type, typename _pair_type::tag_type>;
 };
 
 #endif // _LIBCPP_STD_VER >= 26
